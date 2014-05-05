ae47c0d3ef7176b30fbb8d102c163ee740f04e7b
Jason Ko <jason.ko@mlb.com>
2014-05-05 18:09:48 -0400

    better bamgrid mcollective application plugin

--- lib/mcollective/application/bamgrid.rb
+++ lib/mcollective/application/bamgrid.rb
@@ -1,33 +1,85 @@
+require 'pp'
 class MCollective::Application::Bamgrid<MCollective::Application
-description "Reports on usage for a specific fact"
 
-option :cmd,
-       :description    => "Command to execute",
-       :arguments      => ["-m", "--cmd COMMAND"],
-       :type           => String
+  # copy this file to Mcollective application folder. ie. /usr/libexec/mcollective/mcollective/application
 
-       option :token,
-       :description    => "Auth Token",
-       :arguments      => ["-t", "--token TOKEN"],
-       :type           => String
+  description "Reports on usage for a specific fact"
+
+  option :cmd,
+         :description    => "Command to execute",
+         :arguments      => ["-m", "--cmd COMMAND"],
+         :type           => String
+
+  option :token,
+         :description    => "Auth Token",
+         :arguments      => ["-k", "--token TOKEN"],
+         :type           => String
+
+  option :server_type,
+         :description    => "Server Type",
+         :arguments      => ["-t", "--server_type TYPE"],
+         :type           => String
+
+  option :group,
+         :description    => "Group",
+         :arguments      => ["-t", "--group GROUP"],
+         :type           => String,
+         :validate       => Proc.new {|val| val =~ /^mob$|^dev$|^qa$|^ops$/ ? true : "Invalid Group" }
+
+  option :zone,
+         :description    => "Availability zone",
+         :arguments      => ["-z", "--az ZONE"],
+         :type           => String
 
 
-       def post_option_parser(configuration)
-	    if ARGV.length >= 1
-		configuration[:cmd] = ARGV[0]
-		ARGV.delete_at(0)
-	    else
-		STDERR.puts("No command specified")
-		exit!
-	    end
-	end
+
+  def post_option_parser(configuration)
+    puts ARGV
+    if ARGV.length >= 1
+      configuration[:cmd] = ARGV[0]
+      ARGV.delete_at(0)
+    else
+      STDERR.puts("No command specified")
+      exit!
+    end
+    ARGV.each do |v|
+      puts '==========================='
+      puts v
+      if v =~ /^(.+?)=(.+)$/
+        configuration[:arguments] = [] unless configuration.include?(:arguments)
+        configuration[:arguments] << v
+      else
+        STDERR.puts("Could not parse --arg #{v}")
+      end
+    end
+  end
+
+  def main
+    options[:output_format] = :console
+    options[:collective] = "mcollective"
+    options[:verbose] = true
+    options[:timeout] = 600
+    options[:disctimeout] = 5
+    options[:ttl] = 600
+    #options[:filter] = {"fact"=>[], "compound"=>[], "cf_class"=>[], "identity"=>[], "agent"=>["bamgrid"]}
+    options[:config] = "/etc/mcollective/client.cfg"
+    
+    mc = rpcclient("bamgrid", :options => options)
+    
+    if(configuration[:server_type] != nil)
+      mc.fact_filter("server_type=#{configuration[:server_type]}")
+    end
+    if(configuration[:group] != nil)
+      mc.fact_filter("group=#{configuration[:group]}")
+    end
+
+    #pp options
+    #pp configuration
 
 
-	def main
-	    mc = rpcclient("bamgrid")
-
-	    printrpc mc.runcmd(:cmd => configuration[:cmd], :token => 'LONGSTRING')
-
-	    printrpcstats
-	end
+    #mc = rpcclient("bamgrid")
+    #mc.fact_filter("server_type=controller")
+    printrpc mc.runcmd(:cmd => configuration[:cmd], :token => 'LONGSTRING')
+    printrpcstats
+  end
 end
